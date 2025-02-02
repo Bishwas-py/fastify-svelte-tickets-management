7f82e48b0bbefe8c58e722c745a47b8538838b2e
Bishwas Bhandari <bishwasbh@gmail.com>
2025-02-02 21:37:37 +0545

    chore: logout added

--- svelte-web/src/routes/(auth_required)/+page.svelte
+++ svelte-web/src/routes/(auth_required)/+page.svelte
@@ -3,3 +3,13 @@
 </script>
 <h1>Welcome to SvelteKit, @{data.user.username}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<form action="/logout" method="POST" class="ml-auto text-sm flex justify-center items-center px-1 h-6 sm:px-2 sm:h-8
+                     bg-neutral-100 dark:bg-neutral-800 font-normal whitespace-nowrap
+                        text-neutral-700 dark:text-white dark:outline-0 outline outline-1 outline-indigo-900
+                        shadow focus:ring focus:outline-none rounded-lg mr-4">
+ <button type="submit">
+  Logout
+ </button>
+</form>
---
+++ svelte-web/src/routes/logout/+page.server.ts
@@ -0,0 +1,15 @@
+import {redirect} from '@sveltejs/kit'
+import type {Actions, PageServerLoad} from './$types'
+
+export const load: PageServerLoad = async () => {
+  redirect(302, '/');
+}
+
+export const actions: Actions = {
+  default({cookies}) {
+    cookies.delete('sessionid', {
+      path: '/'
+    })
+    redirect(302, '/login');
+  },
+}
